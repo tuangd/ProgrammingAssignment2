a9b1a60f145be4a2ace081ec2dd7d4341fa903a0
tuangd <tuangd@gmail.com>
2014-09-17 21:29:05 +0700

    small edit 20140917
    edited on description on cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Name: chachematrix.R
+## By: Tuang Dheandhanoo
+## Description: 
+##      In this file I create two function 
+##      the first one will create a list of functions similar to the assignment example
+##      set the value of the vector 
+##      get the value of the vector
+##      set the value of inverse
+##      get the value of inverse
+##      
 
 ## Write a short comment describing this function
 
